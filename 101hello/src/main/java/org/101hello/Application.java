1b33b896d8e52598cc8d7be1b1a740fb21e177b8
ShibamD <>
2025-07-18 16:16:09 +0000

    Project init

---
+++ 101hello/src/main/java/org/101hello/Application.java
@@ -0,0 +1,13 @@
+package org.101hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
